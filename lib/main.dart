8878d10594c879d8d2c47c8ae3b38317f92ba1bf
AlchemistDark <justplus@rambler.ru>
2022-04-30 12:55:52 +0700

    First commit.

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+//import 'package:haystack/haystack.dart';
+import 'package:haystack_client/haystack_client.dart';
+
+void main() async {
+  HSkySparkClient hSSC = HSkySparkClient('http://192.168.1.18:8080/api/demo/', 'su', 'su');
+  //await hSSC.open();
+  hSSC.eval("readAll(site)");
+  print(hSSC.eval("readAll(site)"));
+  hSSC.eval("read(point and his).hisRead(lastWeek)");
+  print(hSSC.eval("read(point and his).hisRead(lastWeek)"));
+  runApp(MyApp(hSSC));
+}
+
+class MyApp extends StatelessWidget {
+  late final HSkySparkClient hSSC;
+  MyApp(this.hSSC);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Halo Vorrrlt',
+      theme: ThemeData(
+        // This is the theme of your application.
+
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: MyHomePage(hSSC, title: 'Halo Vorrrlt'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  late final HSkySparkClient hSSC;
+  MyHomePage(this.hSSC, {Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState(hSSC);
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  late final HSkySparkClient hSSC;
+
+  _MyHomePageState(this.hSSC);
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      print('ghbyn ${hSSC.eval("readAll(site)")}');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Вы сказали 'Халоу Ворлд' раз:",
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        label: Text("Сказать 'Халоу Ворлд'"),
+      ),
+    );
+  }
+}
+
